ce178a1b75d735eaea2a488e95b565378142ff08
Atharva Shinde <atharva09shinde@gmail.com>
2024-08-11 15:40:08 +0530

    updated a value to 141

--- SumJava.java
+++ SumJava.java
@@ -3,7 +3,7 @@ package gitDemo;
 public class SumJava {
 
     public static void main(String[] args) {
-        int a = 1;
+        int a = 141;
         int b = 21;
 
         System.out.println("Addition : " +a+b);
